e02386bd85341fb646a25d9a93e203bfc75f6192
Yousof <157152463+ItsYousof@users.noreply.github.com>
2024-08-11 11:02:58 -0400

    .

--- public/js/homework.js
+++ public/js/homework.js
@@ -1,32 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getDatabase, ref, set, onValue, remove, push } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyC3RuGeo6dCnSljaUbyAns9Gq79vD8jeRo",
-    authDomain: "eduguard-e0c86.firebaseapp.com",
-    databaseURL: "https://eduguard-e0c86-default-rtdb.firebaseio.com",
-    projectId: "eduguard-e0c86",
-    storageBucket: "eduguard-e0c86.appspot.com",
-    messagingSenderId: "760038084932",
-    appId: "1:760038084932:web:92aab577bf287776b0c711",
-    measurementId: "G-LM8YXKCK4H"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-const userID = localStorage.getItem("userID");
-
-if (userID === null) {
-    const newUserID = Math.random().toString(36).substring(7);
-    localStorage.setItem("userID", newUserID);
-    const userRef = ref(db, "users/" + newUserID);
-    set(userRef, {
-        name: localStorage.getItem("userID"),
-        homework: [
-            { title: "This is an example", description: "Press the add button on the bottom right", link: "https://www.google.com" }
-        ]
-    });
+if (localStorage.getItem("homework") === null) {
+    localStorage.setItem("homework", JSON.stringify([]));
 }
 
 function showPopup() {
@@ -37,7 +10,7 @@ function hidePopup() {
     document.getElementById("popup").style.display = "none";
 }
 
-async function addHomework() {
+function addHomework() {
     const title = document.getElementById("title").value;
     const description = document.getElementById("description").value;
     const link = document.getElementById("link").value;
@@ -52,65 +25,52 @@ async function addHomework() {
         </div>
     `;
 
+    const homework = JSON.parse(localStorage.getItem("homework"));
+    homework.push({ title, description, link });
+    localStorage.setItem("homework", JSON.stringify(homework));
+    document.getElementById("homework-cards").appendChild(card);
     hidePopup();
 
-    const homeworkRef = ref(db, "users/" + userID + "/homework");
-    const newHomeworkRef = push(homeworkRef);
-
-    await set(newHomeworkRef, {
-        title: title,
-        description: description,
-        link: link
-    });
-
-    let homeworkList = document.getElementById("homework-cards");
-    homeworkList.appendChild(card);
-
     const trash = card.querySelector("i.bx-trash");
-    trash.addEventListener("click", async function() {
-        await remove(newHomeworkRef);
+    trash.addEventListener("click", function() {
         card.remove();
+        const updatedHomework = JSON.parse(localStorage.getItem("homework"));
+        const index = updatedHomework.findIndex(item => item.title === title && item.description === description && item.link === link);
+        if (index > -1) {
+            updatedHomework.splice(index, 1);
+            localStorage.setItem("homework", JSON.stringify(updatedHomework));
+        }
     });
-
-    location.reload();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    displayHomework();
-
-    document.getElementById("add-btn").addEventListener("click", showPopup);
-    document.getElementById("add-c").addEventListener("click", addHomework);
+    const homework = JSON.parse(localStorage.getItem("homework"));
+    homework.forEach(function(homeworkItem) {
+        plainAdd(homeworkItem.title, homeworkItem.description, homeworkItem.link);
+    });
 });
 
-function displayHomework() {
-    const homeworkRef = ref(db, "users/" + userID + "/homework");
-    onValue(homeworkRef, function(snapshot) {
-        const homeworkList = document.getElementById("homework-cards");
-        homeworkList.innerHTML = ""; // Clear existing list
+function plainAdd(title, description, link) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+        <h1>${title}</h1>
+        <p>${description}</p>
+        <div class="bottom">
+            <a href="${link}">Link</a>
+            <i class='bx bx-trash'></i>
+        </div>
+    `;
+    document.getElementById("homework-cards").appendChild(card);
 
-        snapshot.forEach(function(childSnapshot) {
-            const homework = childSnapshot.val();
-            const card = document.createElement("div");
-            card.classList.add("card");
-            card.innerHTML = `
-                <h1>${homework.title}</h1>
-                <p>${homework.description}</p>
-                <div class="bottom">
-                    <a href="${homework.link}">Link</a>
-                    <i class='bx bx-trash' data-id="${childSnapshot.key}"></i>
-                </div>
-            `;
-
-            // Add delete functionality
-            const trash = card.querySelector("i.bx-trash");
-            trash.addEventListener("click", async function() {
-                const homeworkKey = trash.getAttribute("data-id");
-                const homeworkToDeleteRef = ref(db, `users/${userID}/homework/${homeworkKey}`);
-                await remove(homeworkToDeleteRef);
-                card.remove();
-            });
-
-            homeworkList.appendChild(card); // Add card to the list
-        });
+    const trash = card.querySelector("i.bx-trash");
+    trash.addEventListener("click", function() {
+        card.remove();
+        const updatedHomework = JSON.parse(localStorage.getItem("homework"));
+        const index = updatedHomework.findIndex(item => item.title === title && item.description === description && item.link === link);
+        if (index > -1) {
+            updatedHomework.splice(index, 1);
+            localStorage.setItem("homework", JSON.stringify(updatedHomework));
+        }
     });
 }
